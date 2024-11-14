1ae7d61c1d6120719b9cd705585bc4adfd0020e7
ChathuniMadhusha <chathunionline97@gmail.com>
2024-11-14 11:34:25 +0530

    Init commit

---
+++ src/main/java/lk/childsafe/ChildsafeApplication.java
@@ -0,0 +1,13 @@
+package lk.childsafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChildsafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChildsafeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/childsafe/ChildsafeApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.childsafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChildsafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
