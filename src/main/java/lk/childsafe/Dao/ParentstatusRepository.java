35441a7350a1a4e768d177a1d70bfe250956e633
Bhanuka Swarnajith Rathnayaka <rmbswarnajith@gmail.com>
2024-11-18 12:33:16 +0530

    bug fixed

--- src/main/java/lk/childsafe/Dao/ParentstatusRepository.java
+++ src/main/java/lk/childsafe/Dao/ParentstatusRepository.java
@@ -1,7 +1,6 @@
 package lk.childsafe.Dao;
 
 import lk.childsafe.Entity.ParentStatus;
-import lk.childsafe.Entity.TeacherStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
